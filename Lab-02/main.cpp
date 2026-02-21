c83b0d26e258a7b703c909817da097a1a761b69d
ahmedyar7 <ahmedyar778@gmail.com>
2026-02-21 19:32:29 +0500

    adding more more changes

--- Lab-02/main.cpp
+++ Lab-02/main.cpp
@@ -1,52 +1,41 @@
 #include <cstring>
 #include <fstream>
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
-class LexicalAnalyzer {
+class Lexer {
    private:
-    vector<string> KEYWORDS;
-    vector<string> INDENTIFYERS;
-    vector<string> DIGITS;
-
-    vector<char> OPERATORS;
-    vector<char> DELIMITERS;
-
     bool compare(const char* str1, const char* str2) {
         while (*str1 && *str2 && *str1 == *str2) {
             str1++;
             str2++;
         }
-
         return *str1 == *str2;
     }
 
-    bool findKeywords(char* ch) {
-        const char* keywords[] = {"int",    "float", "double",   "char",
-                                  "void",   "true",  "false",    "bool",
-                                  "return", "break", "continue", "main"};
+    bool findKeywords(const char* ch) {
+        const char* keywords[] = {
+            "int", "main", "bool",    "float",  "double", "continue", "break",
+            "if",  "else", "else if", "friend", "return", "break"};
 
         for (int i = 0; i < sizeof(keywords) / sizeof(keywords[i]); i++) {
             if (compare(ch, keywords[i])) {
                 return true;
             }
         }
+
         return false;
     }
 
    public:
     void scanner() {
         ifstream inFile("./example.txt");
+        ofstream outFile("./output.txt");
 
-        if (!inFile) {
-            cout << "ERROR: Failed to open the file" << endl;
-        }
-
-        char ch;
         int i = 0;
-        char buff[100];
+        char ch;
+        char buff[1024];
 
         while (inFile.get(ch)) {
             if (isspace(ch)) {
@@ -60,21 +49,20 @@ class LexicalAnalyzer {
                 while (inFile.get(ch) && (isalpha(ch) || isdigit(ch))) {
                     buff[i++] = ch;
                 }
-
                 buff[i] = '\0';
                 inFile.putback(ch);
 
                 if (findKeywords(buff)) {
-                    KEYWORDS.push_back(buff);
-                    // cout << "< Keyword, " << buff << " >" << endl;
-                } else {
-                    INDENTIFYERS.push_back(buff);
-                    // cout << "< Idenifyer: " << buff << " >" << endl;
+                    cout << "KEYWORDS: " << buff << endl;
+                }
+
+                else {
+                    cout << "IDENTIFYER: " << buff << endl;
                 }
             }
 
             else if (isdigit(ch)) {
-                i = 0;
+                int i = 0;
                 buff[i++] = ch;
                 bool hasDot = false;
 
@@ -85,67 +73,41 @@ class LexicalAnalyzer {
                     }
                     buff[i++] = ch;
                 }
-
                 buff[i] = '\0';
                 inFile.putback(ch);
 
-                DIGITS.push_back(buff);
-                // cout << "< Digit: " << buff << " >" << endl;
+                cout << "Digit: " << ch << endl;
+
             }
 
-            else if (ch == '+' || ch == '-' || ch == '/' || ch == '%' ||
-                     ch == '^' || ch == '*') {
-                OPERATORS.push_back(ch);
+            else if (ch == '<' || ch == '<=' || ch == '>' || ch == '>=') {
+                char next;
+                inFile.get(next);
+
+                if (next == '=') {
+                } else {
+                }
             }
 
-            else if (ch == '[' || ch == ']' || ch == ';' || ch == '.' ||
-                     ch == '{' || ch == '}' || ch == '(' || ch == ')') {
-                DELIMITERS.push_back(ch);
+            else if (ch == '+' || ch == '-' || ch == '/' || ch == '*' ||
+                     ch == '%') {
+                cout << "OPERATOR: " << ch << endl;
+            }
+
+            else if (ch == '(' || ch == ')' || ch == '[' || ch == ']' ||
+                     ch == '{' || ch == '}' || ch == ';' || ch == '.' ||
+                     ch == ':') {
+                cout << "DELIMITER: " << ch << endl;
             }
         }
 
         inFile.close();
-
-        ofstream Fout("output.txt");
-
-        // all keywords;
-        Fout << "KEYWORDS: " << "{";
-        for (auto keys : KEYWORDS) {
-            Fout << keys << "  ";
-        }
-        cout << "}" << endl;
-        
-
-        // all INDENTIFYERS
-        Fout << "IDENTIFYERS: " << "{";
-        for (auto keys : INDENTIFYERS) {
-            Fout << keys << " ";
-        }
-        Fout << "}" << endl;
-
-        // all digits
-        Fout << "DIGITS: " << "{";
-        for (auto keys : DIGITS) {
-            Fout << keys << " ";
-        }
-        Fout << "}" << endl;
-
-        Fout << "OPERATORS: " << "{";
-        for (auto keys : OPERATORS) {
-            Fout << keys << " ";
-        }
-        Fout << "}" << endl;
-
-        Fout << "DELIMITERS: " << "{";
-        for (auto keys : DELIMITERS) {
-            Fout << keys << " ";
-        }
-        Fout << "}" << endl;
+        outFile.close();
     }
 };
 
 int main() {
-    LexicalAnalyzer().scanner();
+    Lexer().scanner();
 
     return 0;
 }
