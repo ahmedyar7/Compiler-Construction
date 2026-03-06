2c918867ca29cefc60b6e84f5307e93fe3d6f634
ahmedyar7 <ahmedyar778@gmail.com>
2026-03-07 00:05:52 +0500

    adding some practice regarding the adHoc lexer

--- Ad-Hoc-Lexer/practice.cpp
+++ Ad-Hoc-Lexer/practice.cpp
@@ -1,52 +1,119 @@
 #include <cstring>
+#include <fstream>
 #include <iostream>
-
 using namespace std;
 
-const int MAX_STATES = 10;
-const int MAX_CHARS = 256;
-
-class DFA {
+class AdHocLexer {
    private:
-    int startState;
-    int transitions_matrix[MAX_STATES][MAX_CHARS];
-    bool acceptedStates[MAX_STATES];
+    bool compare(const char* str1, const char* str2) {
+        while (*str1 && *str2 && *str1 == *str2) {
+            str1++;
+            str2++;
+        }
+        return *str1 == *str2;
+    }
+    bool findKeywords(const char* kw) {
+        const char* KEYWORDS[] = {"int",   "float",  "bool",     "double",
+                                  "main",  "return", "continue", "true",
+                                  "false", "class",  "include"};
+        for (int i = 0; i < sizeof(KEYWORDS) / sizeof(KEYWORDS[i]); i++) {
+            if (compare(KEYWORDS[i], kw)) {
+                return true;
+            }
+        }
+        return false;
+    }
 
    public:
-    DFA(int start) : startState(start) {
-        for (int i = 0; i < MAX_STATES; i++) {
-            for (int j = 0; j < MAX_CHARS; j++) {
-                transitions_matrix[i][j] = -1;
-            }
-            acceptedStates[i] = -1;
-        }
-    }
+    void lexer(const char* fileName) {
+        ifstream Fin(fileName);
 
-    void transition(int currentState, char input, int nextState) {
-        while (currentState < MAX_STATES && nextState < MAX_STATES) {
-            transitions_matrix[currentState][input] = nextState;
+        if (!Fin) {
+            cout << "Unable to open the file...\n";
+            return;
         }
-    }
 
-    void acceptState(int state) {
-        if (state < MAX_STATES) {
-            acceptedStates[state] = true;
-        }
-    }
-
-    void process(const char* inputStr) {
-        int currentState = startState;
         int i = 0;
+        char ch;
+        char buff[1024];
 
-        while (inputStr[i] != '\0') {
-            currentState = transitions_matrix[currentState][inputStr[i]];
-
-            if (currentState == -1) {
-                acceptedStates[currentState] = false;
+        while (Fin.get(ch)) {
+            // Handling Whitespaces
+            // - Space
+            // - newline
+            // - new cursor line
+            if (isspace(ch)) {
+                continue;
             }
-            i++;
+
+            // Handling variables
+            if (isalpha(ch)) {
+                i = 0;
+                buff[i++] = ch;
+                while (Fin.get(ch) && (isdigit(ch) || isalpha(ch))) {
+                    buff[i++] = ch;
+                }
+                buff[i] = '\0';
+                Fin.putback(ch);
+
+                if (findKeywords(buff)) {
+                    cout << "KW: " << buff << endl;
+                } else {
+                    cout << "ID: " << buff << endl;
+                }
+            }
+
+            if (isdigit(ch)) {
+                i = 0;
+                buff[i++] = ch;
+                bool isDot = false;
+
+                while (Fin.get(ch) && (isdigit(ch) || (ch == '.' && !isDot))) {
+                    if (ch == '.') {
+                        isDot = true;
+                    }
+                    buff[i++] = ch;
+                }
+                buff[i] = '\0';
+                Fin.putback(ch);
+
+                cout << "DIGITIS: " << buff << endl;
+            }
+
+            if (ch == '+' || ch == '-' || ch == '/' || ch == '%' || ch == '*') {
+                cout << "ARITHEMATIC-OP: " << ch << endl;
+            }
+
+            if (ch == '(' || ch == ')' || ch == '[' || ch == ']' || ch == '{' ||
+                ch == '}') {
+                cout << "DELIMITER: " << ch << endl;
+            }
+
+            if (ch == '<' || ch == '>' || ch == '>=' || ch == '<=' ||
+                ch == '==') {
+                char next = ch;
+                Fin.get(next);
+                if (next == '=') {
+                    cout << "relop=" << ch << " = " << endl;
+                } else {
+                    if (ch == '=') {
+                        cout << "RELATION-OP: " << ch << endl;
+                    } else {
+                        cout << "relop=" << ch << " = " << endl;
+                    }
+                }
+            }
+
+            if (ch == '||' || ch == '!' || ch == '&&') {
+                cout << "Conditional_OP: " << ch << endl;
+            }
         }
+        return;
     }
 };
 
-int main() { return 0; }
+int main() {
+    AdHocLexer().lexer("./sample.txt");
+
+    return 0;
+}
